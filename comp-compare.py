d401bc7dfefe04e362a33bf279c88e467848286b
Dacheng Zhao <daczhao123@gmail.com>
2024-02-16 16:43:25 -0500

    Update output

--- comp-compare.py
+++ comp-compare.py
@@ -24,13 +24,17 @@ def get_user_input():
 
 
 def perform_analysis(xlsx_file, sheet_name_old_comp_search, sheet_name_new_comp_search):
+    sheet_name_psg_bench_mark = 'PSG System Benchmark v24.02.16'
     st.write("Analysis will be started")
+
+    psg_df = pd.read_excel(xlsx_file, sheet_name=sheet_name_psg_bench_mark)
+    analysis_row = psg_df[psg_df["Benchmark to compare"] == sheet_name_old_comp_search]
+    analysis_row = analysis_row.iloc[0]
+    old_comp_pool_size = analysis_row['Unnamed: 5']
 
     old_df = pd.read_excel(xlsx_file, sheet_name=sheet_name_old_comp_search)
     old_df["Source ID"] = old_df["Source ID"].astype(str).str.replace("-", "")
-    # Should I use only Old in New/Old column?
     old_cs_data_accepted = old_df[(old_df["New/Old"] == "Old")]
-    # old_cs_data_accepted = old_df
 
     new_df = pd.read_excel(xlsx_file, sheet_name=sheet_name_new_comp_search)
     merged_df_company_name = pd.merge(
@@ -42,9 +46,6 @@ def perform_analysis(xlsx_file, sheet_name_old_comp_search, sheet_name_new_comp_
         how="inner",
     )
 
-    st.write("Same comps appears in new comp search")
-    st.write(merged_df_company_name)
-
     accepted_company_names = merged_df_company_name["Comparable Company"]
     indices_of_accepted_company_names = []
 
@@ -54,12 +55,16 @@ def perform_analysis(xlsx_file, sheet_name_old_comp_search, sheet_name_new_comp_
 
     st.write("Analysis results will be displayed here")
     st.write(
-        f"{len(indices_of_accepted_company_names)} accepted companies in new comp search results position (identical company name) in length of new comp search result of {len(new_df)} and length of old comp search result of {len(old_df)} is",
+        f"{len(indices_of_accepted_company_names)} accepted companies in new comp search results position (identical company name) in length of new comp search result of {len(new_df)} is",
         indices_of_accepted_company_names,
     )
-    st.write(
-        f"{round((len(indices_of_accepted_company_names)/len(old_df)) * 100, 2)}% of accepted comps from old comp search was appeared in new comp search"
-    )
+    st.write("We have the table of comps that displays the comps that appear in both the old and new searches")
+    st.write(merged_df_company_name)
+    st.write(f"There are a total of {len(merged_df_company_name)} comps which are 'accepted' in both old and new searches")
+    st.write(f"The pool size for the new search is {len(new_df)}")
+    st.write(f"The pool size for the old search is {old_comp_pool_size}")
+    st.write(f"There were {len(old_cs_data_accepted)} accepted comps in the old search")
+    st.write(f"The % of accepted comps from the old search that show up in the new search is {round((len(merged_df_company_name)/len(old_cs_data_accepted)) * 100, 2)}%")
 
 
 if __name__ == "__main__":
